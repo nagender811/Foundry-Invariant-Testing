d94e2ab52208339d0a1cf6c167a73bbbc872dee0
Nagender kumar <kumarnagender811@gmail.com>
2026-06-20 22:10:50 +0530

    Refactor MyToken contract and update tests to use ERC20 implementation

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,6 +1,14 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract MyToken {
-    
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+    constructor()ERC20("demo", "dm") {
+
+    }
+
+    function mintToken() public {
+        _mint(address(this), 1000);
+    }
 }
--- test/MyToken.t.sol
+++ test/MyToken.t.sol
@@ -1,9 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {MyToken} from "../src/MyToken.sol";
 
-contract CounterTest is Test {
-   
+contract MyTokenTest is Test {
+    MyToken public token;
+
+    function setUp() public {
+        token = new MyToken();
+    }
+
+    function invariant_totalSupplyShouldBeZeroWhenNoMinting() public view {
+        assertEq(token.totalSupply(), 0);
+    }
 }

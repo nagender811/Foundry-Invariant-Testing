8ed03c5b5c2c381542303e40fb15bb2e9c67abc8
Nagender kumar <kumarnagender811@gmail.com>
2026-06-20 21:22:19 +0530

    Add initial Foundry project setup

---
+++ test/MyToken.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+import {MyToken} from "../src/MyToken.sol";
+
+contract CounterTest is Test {
+   
+}

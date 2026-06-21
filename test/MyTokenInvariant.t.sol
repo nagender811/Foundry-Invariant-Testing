3505eb1084125f6facfb886132e0a6d56fe36425
Nagender kumar <kumarnagender811@gmail.com>
2026-06-21 21:28:47 +0530

    Fix import path and correct MyHandler instantiation in MyTokenInvariantTest

--- test/MyTokenInvariant.t.sol
+++ test/MyTokenInvariant.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {MyToken} from "../src/MyToken.sol";
-import {MyHandler} from "./MyTokenHandler.sol";
+import {MyHandler} from "./MyTokenHandler.t.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
 
 contract MyTokenInvariantTest is StdInvariant, Test {
@@ -13,17 +13,20 @@ contract MyTokenInvariantTest is StdInvariant, Test {
     function setUp() public {
         token = new MyToken();
         token.mintMyToken(1000);
-        handler = new Myhandler(token);
+        
+        handler = new MyHandler(token);
+
+        vm.prank(address(token));
         token.transfer(address(handler), 1000);
 
-        bytes4[] selectors = new bytes()[1];
+        bytes4[] memory selectors = new bytes4[](1);
 
-        selectors[0] = MyHandler.transferTokens.selectors;
-        targetSelector(FuzzSelector({addr:address(handler),selectors.selectors}));
+        selectors[0] = MyHandler.transferTokens.selector;
         targetContract(address(handler));
+        targetSelector(FuzzSelector({addr:address(handler),selectors:selectors}));
     }
 
-    function invariant_totalSupplyShouldBeZeroWhenNoMinting() public view {
-        assertEq(token.totalSupply(), 0);
+    function invariant_totalSupplyShouldBe1000() public view {
+        assertEq(token.totalSupply(), 1000);
     }
 }
